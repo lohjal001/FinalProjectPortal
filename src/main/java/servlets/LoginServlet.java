5e36a8e0c32384aef3a824636b287437d9c33374
Nikolina Vikberg <149555646+nvikberg@users.noreply.github.com>
2024-02-17 14:42:33 +0100

    started a query file.. added some more comments

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/login")
+@WebServlet(urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
 
     @Override
@@ -19,10 +19,12 @@ public class LoginServlet extends HttpServlet {
         //System.out.println();
 
         req.getRequestDispatcher("JSP/login.jsp").forward(req,resp);
-    }
+    } //forwards the request to the JSP file that has the log in form
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //doPost method handles the form submission, retrives users inputs from the request, then we query the database based on usertype (student or teacher)
+
         resp.setContentType("text/html");
         //hämta data från loginForm
         String username = req.getParameter("username");
@@ -31,13 +33,19 @@ public class LoginServlet extends HttpServlet {
 
         //jämför data med databas student o teacher
         if(userType.equals("student")){
-            List data = MySQLConnector.getConnector().selectQuery("studentLogin",username,password);
-            if(!data.isEmpty())resp.getWriter().print("Logged in!");
+            List data = MySQLConnector.getConnector().selectQuery("loginStudent",username,password);
+            if(!data.isEmpty())resp.getWriter().print("Logged in as student!");
             UserBean userBean = new UserBean();
-            //userBean.setUserPrivilage(UserBean.PRIVILAGE_TYPE.user);
+            userBean.setUserPrivilage(UserBean.PRIVILAGE_TYPE.user);
+
+
 
         } else if (userType.equals("teacher")) {
+            List data = MySQLConnector.getConnector().selectQuery("loginTeacher",username,password);
+            if(!data.isEmpty())resp.getWriter().print("Logged in as teacher!");
 
+
+            //after user are logged in, sessions should start
         }
 
         //resp.getWriter().print(username+" "+password+" "+userType);
