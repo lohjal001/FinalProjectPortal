e451a6831b9bdf56a6918036fee5494a1e942a71
lohjal001 <113910139+lohjal001@users.noreply.github.com>
2024-02-21 12:08:14 +0100

    Update usepage

--- src/main/java/servlets/UserPageServlet.java
+++ src/main/java/servlets/UserPageServlet.java
@@ -23,12 +23,19 @@ public class UserPageServlet extends HttpServlet {
 
         @Override
         protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+<<<<<<< Updated upstream
             //This method should pick up query from csv file, send it to the correct jsp userpage depending on the user type that has logged in
             //Cannot reach data yet... and its not working in the POST method (?)
 
 
             UserBean usersBean = req.getSession().getAttribute("userBean") != null ? (UserBean) req.getSession().getAttribute("userBean") : null;
 
+=======
+            UserBean userBean = req.getSession().getAttribute("userBean") != null ? (UserBean) req.getSession().getAttribute("userBean") : null;
+            UserBean usersBean = (UserBean) req.getSession().getAttribute("UserBean"); //getting the login info saved in bean
+            System.out.println(usersBean);
+            if (usersBean != null && usersBean.getUserType().equals(UserBean.USER_TYPE.student) && usersBean.getStateType().equals((UserBean.STATE_TYPE.confirmed))) {
+>>>>>>> Stashed changes
 
           //  UserBean usersBean = (UserBean) req.getSession().getAttribute("UserBean"); //getting the login info saved in bean
 
@@ -45,13 +52,18 @@ public class UserPageServlet extends HttpServlet {
 
                 req.setAttribute("courses", courses);
                 req.getRequestDispatcher("JSP/userPage.jsp").forward(req, resp);
-
+                req.getRequestDispatcher("JSP/fragments/student/studentUserPage.jsp").forward(req, resp);
                 //LinkedList<String[]> data = MySQLConnector.getConnector().selectQuery("EnrolledCoursesOverview");
                 //req.setAttribute("data", data);
+<<<<<<< Updated upstream
                 //req.getRequestDispatcher("JSP/fragments/student/studentUserPage.jsp").forward(req, resp);
                 System.out.println("userpage servlet got here3");
 
                 
+=======
+
+                System.out.println("got here");
+>>>>>>> Stashed changes
 
 
             } else if (usersBean != null && usersBean.getUserType().equals(UserBean.USER_TYPE.teacher) && usersBean.getStateType().equals((UserBean.STATE_TYPE.confirmed))) {
